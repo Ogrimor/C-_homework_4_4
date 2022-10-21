24980646f71a792b508297b78b9b8af34b3d760e
Ogrimor <ogrimors37@gmail.com>
2022-10-21 12:30:54 +0500

    Get sum of digit

---
+++ the_sum_of_the_digits_in_a_number/Program.cs
@@ -0,0 +1,26 @@
+﻿int [] getIntArrey (string s)
+{
+    
+    int[] arr = new int[s.Length];//     
+    for (int i = 0; i < s.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(s.Substring(i, 1));
+    }
+    return arr;
+}
+
+int getSumDigit(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+Console.WriteLine("Введите желаемое число: ");
+string s = Console.ReadLine();
+char[] removeMinus = {'-'};
+string s1 = s.TrimStart(removeMinus);
+int[] arr = getIntArrey(s1);
+Console.WriteLine(getSumDigit(arr));
