41b3442731fa9dd2a6ca64cca21e03ae4f67e295
Ogrimor <ogrimors37@gmail.com>
2022-10-21 11:03:24 +0500

    Math Pow

---
+++ math_pow/Program.cs
@@ -0,0 +1,19 @@
+﻿int getValue(string msg)
+{
+    Console.WriteLine(msg);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int getDegree(int a, int b)
+{
+    int degree = a;
+    for (int i = 1; i < b; i++)
+    {
+        degree *= a;
+    }
+    return degree;
+}
+
+int baseOfDegree = getValue("Введите основание степени: ");
+int degreeValue = getValue("Введите значение степени: ");
+Console.WriteLine(getDegree(baseOfDegree, degreeValue));
